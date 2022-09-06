2c1177ef4e3fe1794e68544450a8ad15a799e13d
Görkem YAVUZ <85451018+grkmyvz@users.noreply.github.com>
2022-09-06 21:01:35 +0300

    Update APA_Takip_Botu.py
    Mail gönderme eklendi.

--- APA_Takip_Botu.py
+++ APA_Takip_Botu.py
@@ -3,6 +3,12 @@ import time
 import json
 import requests
 
+# Mail sistemini aktif etmek için gerekli kütüphaneler
+# NOT : Mail için gerekli kodları aktif etmek için mail kondlarının başında ve sonunda bulunan """ nın başına # koyarak kodları açabilirsiniz. Örnek #"""
+"""
+import smtplib
+"""
+
 # APA nftlerinin datasının bulunduğu json dosyası
 with open('apa_database.json') as f:
     apaData = json.load(f)
@@ -18,6 +24,13 @@ firstStart = True
 apaContractAddress = "0x770a4c7f875fb63013a6db43ff6af9980fceb3b8"
 waitTime = 5
 lastTxTime = ""
+
+# Mail sistemi için gerekli olan bilgiler
+#"""
+mailAddress = "mytestmail3995@gmail.com" # Mail gönderecek adres
+mailAppKey = "zgqzyfnrosbsyzqj" # Mail gönderecek adres için gerekli key
+receiverMail = "gorkem.yavuz.39@hotmail.com" # Maili alacak adres
+#"""
 
 def moralisRequest(query):
     apiURL = query
@@ -42,6 +55,28 @@ while botActive:
                     print("Listing Update -- " + str(nftId) + " id APA nft is " + str(nftPrice) + " avax listed.")
                     print("Rarity : " + nftData["rarity"]) # Bu kısma apa databasesinde bulunan diğer özellikleri de yazdırabilirsiniz. Örneğin : "nftData["rarity"]" owner, token_id, color, fur, cloth, earring, glass, hat, necklace, mouth, rarity_point, rarity
                     print("-------------------------------------------------")
+                    
+                    # Mail almak için gerekli kodlar
+                    """
+                    s = smtplib.SMTP('smtp.gmail.com', 587)
+                    s.starttls()
+                    s.login(mailAddress, mailAppKey)
+                    message = "Listing Update -- " + str(nftId) + " id APA nft is " + str(nftPrice) + " avax listed."
+                    s.sendmail(mailAddress, receiverMail, message)
+                    s.quit()
+                    """
+                    
+                    # Eğer sadece özel nftler için mail almak istiyorsanız bu kısmı kullanın. İstediğiniz özellikleri if kullanımı ile filtreleyebilirsiniz.
+                    """
+                    if nftData["rarity"] == "Rare" or nftData["hat"] == "king": 
+                        s = smtplib.SMTP('smtp.gmail.com', 587)
+                        s.starttls()
+                        s.login(mailAddress, mailAppKey)
+                        message = "Listing Update -- " + str(nftId) + " id APA nft is " + str(nftPrice) + " avax listed."
+                        s.sendmail(mailAddress, receiverMail, message)
+                        s.quit()
+                    """
+                    
                 elif txData["to_address"] == apaContractAddress and len(txData["logs"][0]["data"]) == 642:
                     txLogDataHex = txData["logs"][0]["data"]
                     nftId = int(txLogDataHex[194:258], 16)
